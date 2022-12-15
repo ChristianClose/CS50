e9c9ac971ccbf5d4d39e3985bfeaa2678f42fd8e
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-15 03:47:23 +0000

    Thu, Dec 15, 2022, 3:47 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,7 +4,11 @@
 
 int main(void)
 {
-    int height = get_int("Height: ");
+    int height;
+    do{
+        height = get_int("Height: ");
+    } while(height > 0 && < 9);
+
 
     for(int i = 0; i < height; i++)
     {
