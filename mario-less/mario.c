f27008e2db7c1f777fe3ae886374392ebd13c1ad
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-15 03:50:45 +0000

    Thu, Dec 15, 2022, 3:50 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -7,7 +7,7 @@ int main(void)
     int height;
     do{
         height = get_int("Height: ");
-    } while(height > 0 || height < 9);
+    } while(height > 0 && height < 9);
 
     printf("Stored: %i", height);
 
