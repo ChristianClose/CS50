dd59145b1299887c42faf8bb5ac94b17d43e336c
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-15 04:47:19 +0000

    Thu, Dec 15, 2022, 4:47 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,18 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void rightLeaningTriangle(int height);
-void leftLeaningTriangle(int height);
-void spaces(int height);
 void createTriangles(int height);
 
 int main(void)
 {
     int height;
     do
-        {
-            height = get_int("Height: ");
-        }
+    {
+        height = get_int("Height: ");
+    }
     while (height <= 0 || height > 8);
 
     createTriangles(height);
@@ -20,15 +17,16 @@ int main(void)
 
 }
 
-void createTriangles(height){
-    for(int i = 0; i < height; i++)
+void createTriangles(height)
+{
+    for (int i = 0; i < height; i++)
     {
-        for(int space = (height - i); space > 1; space--)
+        for (int space = (height - i); space > 1; space--)
         {
             printf(" ");
         }
 
-        for(int hashtag = 0; hashtag <= i; hashtag++)
+        for (int hashtag = 0; hashtag <= i; hashtag++)
         {
             printf("#");
 
@@ -43,22 +41,4 @@ void createTriangles(height){
         printf("\n");
     }
 
-}
-
-void rightLeaningTriangle(int height)
-{
-
-}
-
-void leftLeaningTriangle(int height)
-{
-
-}
-
-void spaces(int height){
-    for(int i = 0; i < height; i++)
-    {
-        printf("  ");
-        printf("\n");
-    }
 }
