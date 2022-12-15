655fbd45649e6676dc869c1a27deb83056a87e6d
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-15 21:38:13 +0000

    Thu, Dec 15, 2022, 9:38 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,30 +12,7 @@ int main(void)
 
 void checkCreditCard(long number)
 {
-    int sum = 0;
-    int num = number;
-    int num2 = number / 10;
-    int finalSum = 0;
-    int doubledSum = num2 *2;
-
-    while(number){
-        num = number % 10;
-        sum += num;
-
-        num2 %=  10;
-        doubledSum += num2 * 2;
-
-        number /= 100;
-    }
-
-    finalSum = sum + doubledSum;
-    printf("%i\n", finalSum);
-    printf("%i\n", sum);
-    printf("%i\n", doubledSum);
-
-    if(finalSum % 10 == 0){
-        printf("Valid Card\n");
-    }
+    
 
 
 }
