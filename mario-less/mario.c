ae07b9ad7ef429ff456e5490227da6e5e29ca0e4
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-15 03:33:35 +0000

    Thu, Dec 15, 2022, 3:33 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -9,7 +9,9 @@ int main(void)
     for(int i = 0; i < height; i++)
     {
         for(int j = 0; j < i; j++){
-            
+            for(int k = j; k > 0; k--){
+                printf(" ");
+            }
                 printf("#");
             }
 
