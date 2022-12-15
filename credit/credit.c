1fdcda649c7cd0d6ccd3f0fe4b4b3df1443250d2
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-15 20:35:32 +0000

    Thu, Dec 15, 2022, 8:35 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,12 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void checkCreditCard(long number);
+
 int main(void)
 {
-    long creditCardNum;
-
-    do
-    {}
-
+    long creditCardNum = get_long("Number: ");
 
 }
+
+void checkCreditCard(long number)
+{
+    for(int i = 0; i < )
+}
+
