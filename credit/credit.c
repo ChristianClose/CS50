a0bb0be8e1fce91ef90ae388106fca244daf2446
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-15 23:00:27 +0000

    Thu, Dec 15, 2022, 11:00 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 
 void checkCreditCard(long number);
+int getfirstCCSum(long number);
+int getSecondCCSum(long number);
+int getNumOfDigits(long number);
+long getDivisor(long number);
+string getCCProvidor(long number, long divisor, int sum, int numOfDigits);
 
 int main(void)
 {
@@ -18,6 +23,77 @@ void checkCreditCard(long number)
     long divisor = 10;
     int firstDigit;
     int firstTwoDigits;
+    string providor;
+
+    // while(temp)
+    // {
+    //     int lastDigit = temp % 10;
+    //     sum += lastDigit;
+    //     temp /= 100;
+    // }
+
+    sum = getFirstCCSum(number);
+    sum += getSecondCCSum(number);
+    numOfDigits = getNumOfDigits(number);
+    divisor = getDivisor(number);
+    providor = getCCProvidor(number, divisor, sum, numOfDigits);
+
+    // temp = number / 10;
+
+    // while (temp)
+    // {
+    //     int lastDigit = temp % 10;
+    //     int doubled = lastDigit * 2;
+    //     sum += (doubled % 10) + (doubled / 10);
+    //     temp /= 100;
+    // }
+
+    // temp = number;
+
+    // while (temp)
+    // {
+    //     temp /= 10;
+    //     numOfDigits++;
+    // }
+
+    // for (int i = 0; i < numOfDigits - 2; i++)
+    // {
+    //     divisor *= 10;
+    // }
+
+    // firstDigit = number / divisor;
+    // firstTwoDigits = number / (divisor / 10);
+
+    // if (sum % 10 == 0)
+    // {
+    //     if (firstDigit == 4 && (numOfDigits == 13 || numOfDigits == 16))
+    //     {
+    //         printf("VISA\n");
+    //     }
+    //     else if ((firstTwoDigits == 34 || firstTwoDigits == 37) && numOfDigits == 15)
+    //     {
+    //         printf("AMEX\n");
+    //     }
+    //     else if ((firstTwoDigits > 50 && firstTwoDigits < 56) && numOfDigits == 16)
+    //     {
+    //         printf("MASTERCARD\n");
+    //     }
+    //     else
+    //     {
+    //         printf("INVALID\n");
+    //     }
+    // }
+    // else
+    // {
+    //     printf("INVALID\n");
+    // }
+
+}
+
+int getFirstCCSum(long number)
+{
+    long temp = number;
+    int sum = 0;
 
     while(temp)
     {
@@ -26,9 +102,15 @@ void checkCreditCard(long number)
         temp /= 100;
     }
 
-    temp = number / 10;
+    return sum;
+}
 
-    while(temp)
+int getSecondCCSum(long number)
+{
+    long temp = number / 10;
+    int sum = 0;
+
+    while (temp)
     {
         int lastDigit = temp % 10;
         int doubled = lastDigit * 2;
@@ -36,21 +118,40 @@ void checkCreditCard(long number)
         temp /= 100;
     }
 
-    temp = number;
+    return sum;
+}
 
-    while(temp)
+int getNumOfDigits(long number)
+{
+    long temp = number;
+    int numOfDigits = 0;
+
+    while (temp)
     {
         temp /= 10;
         numOfDigits++;
     }
 
-    for(int i = 0; i < numOfDigits - 2; i++)
+    return numOfDigits;
+
+}
+
+long getDivisor(long number)
+{
+    long divisor = 10;
+
+    for (int i = 0; i < numOfDigits - 2; i++)
     {
         divisor *= 10;
     }
 
-    firstDigit = number / divisor;
-    firstTwoDigits = number / (divisor / 10);
+    return divisor;
+}
+
+string getCCProvidor(long number, long divisor, int sum, int numOfDigits)
+{
+    int firstDigit = number / divisor;
+    int firstTwoDigits = number / (divisor / 10);
 
     if (sum % 10 == 0)
     {
@@ -75,8 +176,5 @@ void checkCreditCard(long number)
     {
         printf("INVALID\n");
     }
-
-
-
 }
 
