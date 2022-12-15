3a4ade630a3f75cb466776d2f313db662bec5532
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-15 04:54:44 +0000

    Thu, Dec 15, 2022, 4:54 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -38,6 +38,12 @@ int main(void)
 int get_cents(void)
 {
     // TODO
+    int cents;
+    do
+    {
+        cents = get_int("Change owed: ");
+    }
+    while(cents <=0 || cents >);
     return 0;
 }
 
