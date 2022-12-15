ec99d240d33ce36f3766b53538d70d4425bb0050
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-15 04:15:17 +0000

    Thu, Dec 15, 2022, 4:15 AM +00:00

---
+++ mario-more/mario.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+        {
+            height = get_int("Height: ");
+        }
+    while (height <= 0 || height > 8);
+
+    for(int i = 0; i < height; i++){
+        for(int space = (height - 1); space > 1; i--){
+            printf(" ");
+        }
+
+        for(int hashtag = 0; hashtag = height; hashtag++){
+            printf("#");
+        }
+    }
+
+}
