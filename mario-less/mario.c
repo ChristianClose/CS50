eff7078254b082399c3ad7f8806d6562c4751d10
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-15 02:50:29 +0000

    Thu, Dec 15, 2022, 2:50 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -5,9 +5,18 @@ string createTriangle(int base);
 
 int main(void)
 {
-
+    string triangle = createTriangle(8);
+    printf("%s", triangle);
 }
 
-string createTriangle(int base){
-    
+string createTriangle(int base)
+{
+    string triangle = "";
+
+    for(int i = 0; i < base; i++){
+        triangle += '#';
+        printf("%s", triangle);
+    }
+
+    return triangle;
 }
