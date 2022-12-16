853a7a221f390cf9d226a807d1f25209126e15e4
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-16 07:01:24 +0000

    Fri, Dec 16, 2022, 7:01 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 string encrypt(int key, string plainText);
 
@@ -20,15 +21,19 @@ string encrypt(int key, string plainText)
     string cipherText = "";
 
     for(int i = 0; i < strlen(plainText); i++){
-        char letter = plainText[i];
 
-        if(letter > 'Z' && < 'a')
+        char letter = plainText[i+key];
+
+        if(letter > 'Z' &&  letter < 'a')
         {
             letter = 'A' + (letter - 'Z');
         }
         else if(letter > 'z'){
-            letter = 'a' + (letter - "z");
+            letter = 'a' + (letter - 'z');
         }
+
+        printf("%c\n", letter);
+
 
         cipherText += letter;
     }
