d46be5c07c8f911618588af5f6bce97538dff0ad
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-16 09:11:09 +0000

    Fri, Dec 16, 2022, 9:11 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -10,26 +10,27 @@ bool only_digits(string s);
 int main(int argc, string argv[])
 {
     int key = 0;
-    if(argc >= 2 && only_digits(argv[1]))
+
+    //check if there are arguments and if they are valid
+    if (argc >= 2 && only_digits(argv[1]))
     {
         key = atoi(argv[1]);
     }
 
-
-    if(key <= 0)
+    if (key <= 0)
     {
         printf("USAGE: ./caesar key \n");
         return 1;
     }
 
-    if(argc < 2 || argc > 2)
+    if (argc < 2 || argc > 2)
     {
         printf("USAGE: ./caesar key \n");
         return 1;
     }
 
     string plainText = get_string("plaintext: ");
-    char* cipherText = cipherText = encrypt(key, plainText);
+    char *cipherText = cipherText = encrypt(key, plainText);
 
     printf("ciphertext: %s\n", cipherText);
     printf("key: %i\n", key);
@@ -40,43 +41,39 @@ char *encrypt(int key, string plainText)
 {
 
     int plainTextSize = (int)strlen(plainText);
-    char cipherText[plainTextSize+1];
+    char cipherText[plainTextSize + 1];
 
-    for(int i = 0; i < strlen(plainText); i++)
+    for (int i = 0; i < strlen(plainText); i++)
     {
         int letter;
-        if(plainText[i] >= 'A' && plainText[i] <= 'z'){
+        if (plainText[i] >= 'A' && plainText[i] <= 'z')
+        {
             letter = plainText[i] + key;
-        } else {
+        }
+        else
+        {
             letter = plainText[i];
         }
 
-        if(letter > 'Z' &&  letter < 'a')
+        if (letter > 'Z' &&  letter < 'a')
         {
             letter = 'A' + (letter - 'Z') - 1;
         }
 
-        if(letter > 'z')
+        if (letter > 'z')
         {
             letter = 'a' + (letter - 'z') - 1;
-            for(int j = 'a'; j < 'z'; j++)
+            for (int j = 'a'; j < 'z'; j++)
             {
-                if(letter > 'z'){
+                if (letter > 'z')
+                {
                     letter = 'a' + (letter - 'z') - 1;
                 }
             }
 
         }
 
-        printf("plainText Letter value: %d\n", plainText[i]);
-        printf("plainText Letter: %c\n\n", plainText[i]);
-
-        printf("Letter Value: %d\n", letter);
-        printf("Letter: %c\n\n", letter);
-       // printf("plainText[i]: %c\n", plainText[i]);
-
-
-         cipherText[i] = letter;
+        cipherText[i] = letter;
     }
 
     cipherText[plainTextSize] = '\0';
@@ -86,9 +83,12 @@ char *encrypt(int key, string plainText)
     return cipherTextPointer;
 }
 
-bool only_digits(string s){
-    for(int i = 0; i < strlen(s); i++){
-        if(!isdigit(s[i])){
+bool only_digits(string s)
+{
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (!isdigit(s[i]))
+        {
             return false;
         }
 
