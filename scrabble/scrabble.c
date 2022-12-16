4f8f7704b43c742af094d74678d547f00204f451
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-16 04:05:03 +0000

    Fri, Dec 16, 2022, 4:05 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -39,12 +39,9 @@ int compute_score(string word)
     word = lowercase(word);
     size_t alphabetLength = sizeof(ALPHABET) / sizeof (ALPHABET[0]);
 
-    printf("%lu\n", alphabetLength);
-    printf("%lu\n", strlen(ALPHABET));
-
     for(int i = 0; i < strlen(word); i++)
     {
-        for (int j = 0; j < strlen(ALPHABET); j++)
+        for (int j = 0; j < alphabetLength; j++)
         {
             if(word[i] == ALPHABET[j])
             {
