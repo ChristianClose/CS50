0e49b5d5fe944ab8a8176006a9ee4d9a34be29ff
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-16 23:33:27 +0000

    Fri, Dec 16, 2022, 11:33 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -135,6 +135,8 @@ string validateKey(string key)
        {
             for(int j = 1; j < strlen(key); j++)
             {
+                printf("%c\n", key[i]);
+                printf("%c\n\n", key[j]);
                 if(j == i){
                     j++;
                 }
