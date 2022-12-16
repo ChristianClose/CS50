4f9d14f2bcb01c55b3d410e5094c08babf54bc7f
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-16 06:19:27 +0000

    Fri, Dec 16, 2022, 6:19 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+string encrypt(int key, string plainText);
 
 int main(int argc, string argv[])
 {
-    
+    int key = atoi(argv[0]);
+    string plainText = get_string("plaintext: ");
+
 }
+
+string encrypt(int key, string plainText)
+{
+    for(int i = )
+}
+
