fd00272915ad5a60d0a815508323057442a479c0
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-16 19:33:55 +0000

    Fri, Dec 16, 2022, 7:33 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    int key = 0;
+    if(argc == 2){
+        if(strlen(argv[1]) == 26){
+            key = atoi(argv[i]);
+        } else {
+            printf("Key must contain 26 characters.");
+        }
+
+    } else {
+        printf("Usage: ./substitution key);
+    }
+
 
 }
