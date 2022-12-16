d681a25c5ab714efe0f512f301e887d08836e9a8
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-16 03:21:41 +0000

    Fri, Dec 16, 2022, 3:21 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,6 +5,7 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+char ALPHABET[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
 int compute_score(string word);
 string lowercase(string word);
@@ -20,8 +21,17 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
-
-    printf("%i\n", score1);
+    if(score1 > score2)
+    {
+        printf("Player 1 wins!");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!");
+    } else
+    {
+        printf("Tie!);
+    }
 }
 
 int compute_score(string word)
@@ -29,13 +39,12 @@ int compute_score(string word)
     int score = 0;
     word = lowercase(word);
 
-    char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     // TODO: Compute and return score for string
     for(int i = 0; i < sizeof word; i++)
     {
-        for (int j = 0; j < sizeof alphabet; j++)
+        for (int j = 0; j < sizeof ALPHABET; j++)
         {
-            if(word[i] == alphabet[j])
+            if(word[i] == ALPHABET[j])
             {
                 score += POINTS[j];
             }
