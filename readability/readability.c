6d3dbd96f5eea0cb3471be8a1b5435e8e293047e
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-16 04:21:19 +0000

    Fri, Dec 16, 2022, 4:21 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,8 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+int countLetters(string text);
 
 int main(void)
 {
-    
+    string text = get_string("Text: \n");
 
+
+}
+
+int countLetters(string text)
+{
+    for(int i =0; i < strlen(text); i++){
+        if()
+    }
 }
