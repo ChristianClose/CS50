a8b35e223dcc753a3de5e001e16f6374ec62815f
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-17 02:40:37 +0000

    Sat, Dec 17, 2022, 2:40 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -99,32 +99,27 @@ void print_winner(void)
         }
     }
 
-    // for (int i = 0; i < tieCount; i++)
-    // {
-    //     if (tie[i].votes > winner.votes)
-    //     {
-    //         winner = tie[i];
-    //         tie[i] = tie[i + 1];
-    //     }
-    //     else
-    //     {
-    //         tie[i] = tie[i + 1];
-    //     }
-
-    // }
-
-
-    if (tieCount == 0)
+    for (int i = 0; i < tieCount; i++)
     {
-        printf("%s\n", winner.name);
-    }
-    else
-    {
-        printf("%s\n", winner.name);
-        for (int i = 0; i < tieCount - 1; i++)
+        if (tie[i].votes > winner.votes)
         {
-            printf("%s\n", tie[i].name);
+            winner = tie[i];
+            tie[i] = tie[i + 1];
         }
+        else
+        {
+            tie[i] = tie[i + 1];
+        }
+
+    }
+
+
+
+    printf("%s\n", winner.name);
+
+    for (int i = 0; i < tieCount - 1; i++)
+    {
+        printf("%s\n", tie[i].name);
     }
 
     return;
