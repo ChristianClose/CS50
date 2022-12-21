c0262d25efebb766b90d4e9901eb64932d79ba7c
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-21 21:54:24 +0000

    Wed, Dec 21, 2022, 9:54 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -169,7 +169,7 @@ void sort_pairs(void)
     // TODO
     for(int i = 0; i < candidate_count; i++)
     {
-        for(int j = i; j < candidate_count; j++)
+        for(int j = 0; j < candidate_count; j++)
         {
 
             if(preferences[i][j] < preferences[j][i])
