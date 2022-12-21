58d738ff10af835b3585045cc4fc0d9d221d5cfc
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-21 23:06:18 +0000

    Wed, Dec 21, 2022, 11:06 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -160,9 +160,12 @@ void sort_pairs(void)
     pair temp;
     for(int i = 0; i < pair_count; i++)
     {
-        for(int j = i + 1; j < pair_count - 1; j++)
+        int strength = preferences[pairs[i].winner][pairs[i].loser] - preferences[pairs[i].loser][pairs[i].winner];
+        for(int j = i + 1; j < pair_count; j++)
         {
-            if(pairs[j].winner > pairs[i].winner){
+            int strength2 = preferences[pairs[j].winner][pairs[j].loser] - preferences[pairs[j].loser][pairs[j].winner];
+            if(strength < strength2)
+            {
                 temp = pairs[i];
                 pairs[i] = pairs[j];
                 pairs[j] = temp;
