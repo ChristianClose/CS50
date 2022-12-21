72249e7653287f0aaa4f0139659706e65d3af0c7
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-21 23:57:28 +0000

    Wed, Dec 21, 2022, 11:57 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -212,5 +212,12 @@ void lock_pairs(void)
 void print_winner(void)
 {
     // TODO
-    return;
+    winner = candidate[0];
+    for(int i = 0; i < candidate_count; i++)
+    {
+        for(int j = 0; j < candidate_count; j++)
+        {
+            if(!locked[i][j])
+        }
+    }
 }
