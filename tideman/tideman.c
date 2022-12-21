f6236affdbe493570996bd50d8dd842bcf3e0bb0
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-21 21:35:25 +0000

    Wed, Dec 21, 2022, 9:35 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -135,8 +135,15 @@ void add_pairs(void)
         {
             if(preferences[i][j] > preferences[j][i])
             {
-                pairs[i].winner = preferences[i][j];
-                pairs[i].loser = preferences[j][i];
+                pairs[pair_count].winner = preferences[i][j];
+                pairs[pair_count].loser = preferences[j][i];
+                pair_count++;
+            }
+
+            if(preferences[i][j] < preferences[j][i])
+            {
+                pairs[pair_count].winner = preferences[j][i];
+                pairs[pair_count].loser = preferences[i][j];
                 pair_count++;
             }
 
