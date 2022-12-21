38be22ea57ce2768c5574b7413446580d850a653
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2022-12-21 06:14:30 +0000

    Wed, Dec 21, 2022, 6:14 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,6 @@ bool vote(int voter, int rank, string name)
 {
 
     for (int i = 0; i < candidate_count; i++){
-        printf("name: %i\n", strcmp(candidates[i].name, name));
         if(i == candidate_count - 1 && strcmp(name, candidates[i].name) != 0)
         {
                return false;
