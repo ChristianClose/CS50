1cc43331884fca85194258ac6f2c3d75d1e8d031
Christian Close <73373919+ChristianClose@users.noreply.github.com>
2023-04-01 01:15:22 +0000

    Fri, Mar 31, 2023, 8:15 PM -05:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -89,7 +89,6 @@ int main(int argc, string argv[])
 
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
-        printf("%s", choice);
         for(int j = 0; j < sizeof(status); j++)
         {
             status[j] = WRONG;
@@ -116,6 +115,10 @@ int main(int argc, string argv[])
     if(won)
     {
         printf("You won!");
+    }
+    else
+    {
+        printf("The word I was looking for was %s.\n", choice);
     }
     // that's all folks!
     return 0;
@@ -163,7 +166,6 @@ int check_word(string guess, int wordsize, int status[], string choice)
                     status[j] = CLOSE;
                 }
             }
-            printf("%d\n", status[j]);
             score += status[j];
         }
 
